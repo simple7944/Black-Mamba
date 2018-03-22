e2a8f97357843da46ddef03cee14b687828930b5
Alexander Pokinchereda <simple7944@gmail.com>
2018-03-22 21:40:23 +0200

    v0.0005

--- wsgi.py
+++ wsgi.py
@@ -5,12 +5,12 @@ from viberbot.api.bot_configuration import BotConfiguration
 
 application = Flask(__name__)
 
-viber = Api(BotConfiguration(
-    name='PythonSampleBot',
-    avatar='http://media.wplm.pl/pictures/wp/photo/2012-10/414/414/hammer_and_sicklesvg.jpg',
-    auth_token='47903e4860a7d433-88367fbaec064c17-feb50e42a2f20d9c'
-))
-viber.set_webhook('http://black-mamba-black-mamba.1d35.starter-us-east-1.openshiftapps.com:8080/')
+# viber = Api(BotConfiguration(
+#     name='PythonSampleBot',
+#     avatar='http://media.wplm.pl/pictures/wp/photo/2012-10/414/414/hammer_and_sicklesvg.jpg',
+#     auth_token='47903e4860a7d433-88367fbaec064c17-feb50e42a2f20d9c'
+# ))
+# viber.set_webhook('http://black-mamba-black-mamba.1d35.starter-us-east-1.openshiftapps.com:8080/')
 
 @application.route("/")
 def hello():
