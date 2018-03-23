a55aa87e8bb231cc71a122eebd0666b9c02ead50
Alexander Pokinchereda <simple7944@gmail.com>
2018-03-23 15:52:11 +0200

    v0.01

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,29 @@
 from flask import Flask, Response, request
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
-
+from viberbot.api.viber_requests import viber_request
 
 application = Flask(__name__)
 
 
 
 
-# viber = Api(BotConfiguration(
-#     name='PythonSampleBot',
-#     avatar='http://media.wplm.pl/pictures/wp/photo/2012-10/414/414/hammer_and_sicklesvg.jpg',
-#     auth_token='47903e4860a7d433-88367fbaec064c17-feb50e42a2f20d9c'
-# ))
+viber = Api(BotConfiguration(
+    name='PythonSampleBot',
+    avatar='http://media.wplm.pl/pictures/wp/photo/2012-10/414/414/hammer_and_sicklesvg.jpg',
+    auth_token='47903e4860a7d433-88367fbaec064c17-feb50e42a2f20d9c'
+))
 
-# viber.set_webhook('https://black-mamba-black-mamba.1d35.starter-us-east-1.openshiftapps.com:8080/')
+
 
 @application.route("/", methods=['POST'])
 def hello():
 
+    message = viber_request.message
+    # lets echo back
+    viber.send_messages(viber_request.sender.id, [
+        message
+    ])
     return Response(status=200)
 
 if __name__ == "__main__":
