fea4bbde4bda760b348f2ff4d26413fb36dd5716
Alexander Pokinchereda <simple7944@gmail.com>
2018-03-23 16:06:09 +0200

    v0.03

--- wsgi.py
+++ wsgi.py
@@ -26,6 +26,12 @@ viber = Api(BotConfiguration(
 
 @application.route("/", methods=['POST'])
 def hello():
+    # logger.debug("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
     viber_request = viber.parse_request(request.get_data())
 
     if isinstance(viber_request, ViberMessageRequest):
@@ -34,6 +40,12 @@ def hello():
         viber.send_messages(viber_request.sender.id, [
             message
         ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    
+        # logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
     return Response(status=200)
 
